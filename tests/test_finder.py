4269944355797636d9c0572c9f38a9d789ac6d6c
Lev Vereshchagin <mail@vrslev.com>
2024-07-07 22:37:16 +0300

    Update

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -10,6 +10,17 @@ from auto_typing_final.finder import ImportsResult, get_global_imports
         ("import typing", ImportsResult(module_aliases={"typing"}, has_from_import=False)),
         ("import typing, platform", ImportsResult(module_aliases={"typing"}, has_from_import=False)),
         ("import typing, platform\nimport typing", ImportsResult(module_aliases={"typing"}, has_from_import=False)),
+        ("from typing import Final", ImportsResult(module_aliases=set(), has_from_import=True)),
+        ("from typing import Final as Final", ImportsResult(module_aliases=set(), has_from_import=False)),
+        ("from typing import Final as F", ImportsResult(module_aliases=set(), has_from_import=False)),
+        ("import typing; from typing import Final", ImportsResult(module_aliases={"typing"}, has_from_import=True)),
+        ("import typing as tp", ImportsResult(module_aliases={"tp"}, has_from_import=False)),
+        (
+            "import typing as tp\nimport typing as tt\nimport typing\n"
+            "from typing import Final as F\nfrom typing import Final",
+            ImportsResult(module_aliases={"tp", "tt", "typing"}, has_from_import=True),
+        ),
+        ("import typing as tp\nimport typing as tt", ImportsResult(module_aliases={"tp", "tt"}, has_from_import=False)),
     ],
 )
 def test_get_global_imports(source: str, result: ImportsResult) -> None:
