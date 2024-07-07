4f900e7d4a337a4db2705add7c2468fe6da05628
Lev Vereshchagin <mail@vrslev.com>
2024-07-08 00:06:40 +0300

    Add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -14,6 +14,7 @@ from auto_typing_final.transform import IMPORT_STYLES_TO_IMPORT_CONFIGS, ImportC
         ("a: {} = 1", "a: {} = 1"),
         ("a: int = 1", "a: {}[int] = 1"),
         ("a: typing.Annotated[int, 'hello'] = 1", "a: {}[typing.Annotated[int, 'hello']] = 1"),
+        ("a: list[int] = 1", "a: {}[list[int]] = 1"),
         ("b = 1\na = 2\nb = 3", "b = 1\na: {} = 2\nb = 3"),
         ("b = 1\nb = 2\na = 3", "b = 1\nb = 2\na: {} = 3"),
         ("a = 1\nb = 2\nb = 3", "a: {} = 1\nb = 2\nb = 3"),
@@ -653,4 +654,131 @@ def f():
 def test_add_import(case: str) -> None:
     before, _, after = case.partition("---")
     import_config = IMPORT_STYLES_TO_IMPORT_CONFIGS["typing-final"]
+    assert transform_file_content(before.strip(), import_config=import_config) == after.strip()
+
+
+@pytest.mark.parametrize(
+    ("case", "import_config"),
+    [
+        (
+            """
+from typing import Final
+
+def f():
+    a: Final = 1
+---
+import typing
+from typing import Final
+
+def f():
+    a: typing.Final = 1
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["typing-final"],
+        ),
+        (
+            """
+def f():
+    a: Final = 1
+    b: typing.Final = 2
+    c: Final = 3
+---
+import typing
+def f():
+    a: typing.Final = 1
+    b: typing.Final = 2
+    c: typing.Final = 3
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["typing-final"],
+        ),
+        (
+            """
+import typing
+
+def f():
+    a: Final = 1
+    b: typing.Final = 2
+    c: Final = 3
+---
+from typing import Final
+import typing
+
+def f():
+    a: Final = 1
+    b: Final = 2
+    c: Final = 3
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["final"],
+        ),
+        (
+            """
+import typing as tp
+
+def f():
+    a: tp.Final = 1
+---
+import typing
+import typing as tp
+
+def f():
+    a: typing.Final = 1
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["typing-final"],
+        ),
+        (
+            """
+import typing as tp
+
+def f():
+    a: tp.Final = 1
+---
+from typing import Final
+import typing as tp
+
+def f():
+    a: Final = 1
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["final"],
+        ),
+        (
+            """
+import typing as tp
+
+def f():
+    a: tp.Final = 1
+    a: tp.Final = 2
+    b = 1
+---
+import typing
+import typing as tp
+
+def f():
+    a = 1
+    a = 2
+    b: typing.Final = 1
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["typing-final"],
+        ),
+        (
+            """
+import typing as tp
+
+def f():
+    a: tp.Final = 1
+    a: tp.Final = 2
+    b = 1
+---
+from typing import Final
+import typing as tp
+
+def f():
+    a = 1
+    a = 2
+    b: Final = 1
+""",
+            IMPORT_STYLES_TO_IMPORT_CONFIGS["final"],
+        ),
+    ],
+)
+def test_different_styles(case: str, import_config: ImportConfig) -> None:
+    before, _, after = case.partition("---")
     assert transform_file_content(before.strip(), import_config=import_config) == after.strip()
