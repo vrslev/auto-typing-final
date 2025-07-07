4369129f5e9d59f75c04eaf2f90c7387b593b48d
Lev Vereshchagin <mail@vrslev.com>
2025-07-07 17:28:02 +0300

    Organize test utilities into conftest.py and streamline imports

--- tests/test_transform_global_vars.py
+++ tests/test_transform_global_vars.py
@@ -1,47 +1,10 @@
-import pathlib
-import typing
 from typing import Final
 
 import pytest
 
 from auto_typing_final.main import transform_file_content
-from auto_typing_final.transform import IMPORT_STYLES_TO_IMPORT_CONFIGS, ImportConfig
-
-
-def parse_md_test_cases(file_name: str) -> list[str]:
-    md_test = (pathlib.Path(__file__).parent / "md_tests" / file_name).read_text()
-    test_cases: Final = []
-    while True:
-        index_before = md_test.find("```python")
-        if index_before == -1:
-            break
-        case_with_tail = md_test[index_before:].removeprefix("```python")
-        index_after = case_with_tail.find("```")
-        if index_after == -1:
-            break
-        test_cases.append(case_with_tail[:index_after].strip())
-        md_test = case_with_tail[index_after:].removeprefix("```")
-    return test_cases
-
-
-def assert_md_test_case_transformed(*, test_case: str, transformed_result: str, import_config: ImportConfig) -> None:
-    for one_before_line, one_after_line in zip(
-        test_case.splitlines(),
-        transformed_result.removeprefix(import_config.import_text + "\n").splitlines(),
-        strict=True,
-    ):
-        if "# insert" in one_before_line:
-            assert import_config.import_identifier in one_after_line
-        elif "# remove" in one_before_line:
-            assert "Final" in one_before_line
-            assert import_config.import_text not in one_after_line
-        else:
-            assert one_before_line == one_after_line
-
-
-@pytest.fixture(params=IMPORT_STYLES_TO_IMPORT_CONFIGS.values())
-def import_config(request: pytest.FixtureRequest) -> ImportConfig:
-    return typing.cast(ImportConfig, request.param)
+from auto_typing_final.transform import ImportConfig
+from tests.conftest import assert_md_test_case_transformed, parse_md_test_cases
 
 
 @pytest.mark.parametrize("case", parse_md_test_cases("global_vars_enabled.md"))
