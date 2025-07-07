ba238e7d4e711a3a686662b378a5b2ce655bb86a
Lev Vereshchagin <mail@vrslev.com>
2025-07-07 18:05:22 +0300

    Simplify and update test cases for ignoring comments and global variables

---
+++ tests/md_tests/ignore_comment.md
@@ -0,0 +1,37 @@
+# 1
+
+```python
+VAR_WITH_COMMENT = 1 # some comment  # insert
+```
+
+# 1
+
+```python
+IGNORED_VAR = 1 # auto-typing-final: ignore
+```
+
+# 1
+
+```python
+IGNORED_VAR: Final = 1 # auto-typing-final: ignore
+```
+
+# 1
+
+```python
+IGNORED_VAR: Final = 1 # auto-typing-final: ignore  # some comment
+```
+
+# 1
+
+```python
+def foo():
+    a = 1  # insert
+```
+
+# 1
+
+```python
+def foo():
+    a = 1  # auto-typing-final: ignore
+```
--- tests/test_ignore_comment.py
+++ tests/test_ignore_comment.py
@@ -3,26 +3,25 @@ from typing import Final
 import pytest
 
 from auto_typing_final.main import transform_file_content
-from auto_typing_final.transform import IMPORT_STYLES_TO_IMPORT_CONFIGS
+from auto_typing_final.transform import ImportConfig
+from tests.conftest import assert_md_test_case_transformed, parse_md_test_cases
 
 
-@pytest.mark.parametrize(
-    ("before", "after"),
-    [
-        ("VAR_WITH_COMMENT = 1 # some comment", "VAR_WITH_COMMENT: Final = 1 # some comment"),
-        ("IGNORED_VAR = 1 # auto-typing-final: ignore", "IGNORED_VAR = 1 # auto-typing-final: ignore"),
-        ("IGNORED_VAR: Final = 1 # auto-typing-final: ignore", "IGNORED_VAR: Final = 1 # auto-typing-final: ignore"),
-        (
-            "IGNORED_VAR: Final = 1 # auto-typing-final: ignore  # some comment",
-            "IGNORED_VAR: Final = 1 # auto-typing-final: ignore  # some comment",
-        ),
-        ("def foo():\n a = 1", "def foo():\n a: Final = 1"),
-        ("def foo():\n a = 1  # auto-typing-final: ignore", "def foo():\n a = 1  # auto-typing-final: ignore"),
-    ],
-)
-def test_ignore_comment(before: str, after: str) -> None:
-    import_config: Final = IMPORT_STYLES_TO_IMPORT_CONFIGS["final"]
+@pytest.mark.parametrize("case", parse_md_test_cases("ignore_comment.md"))
+def test_ignore_comment_global_vars_enabled(case: str, import_config: ImportConfig) -> None:
     result: Final = transform_file_content(
-        f"{import_config.import_text}\n" + before.strip(), import_config=import_config, ignore_global_vars=False
+        f"{import_config.import_text}\n" + case,
+        import_config=import_config,
+        ignore_global_vars=False,
     )
-    assert result == f"{import_config.import_text}\n" + after.strip()
+    assert_md_test_case_transformed(test_case=case, transformed_result=result, import_config=import_config)
+
+
+def test_ignore_comment_global_vars_disabled(import_config: ImportConfig) -> None:
+    case: Final = "VAR_WITH_COMMENT = 1 # some comment"
+    result: Final = transform_file_content(
+        f"{import_config.import_text}\n{case}",
+        import_config=import_config,
+        ignore_global_vars=True,
+    )
+    assert_md_test_case_transformed(test_case=case, transformed_result=result, import_config=import_config)
