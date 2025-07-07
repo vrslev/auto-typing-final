d2052299de0571b7ee7bf09de920381a35ecb485
Lev Vereshchagin <mail@vrslev.com>
2025-07-07 17:26:14 +0300

    Update test cases for global vars with ignore flag

--- tests/test_transform_global_vars.py
+++ tests/test_transform_global_vars.py
@@ -52,9 +52,9 @@ def test_global_vars_enabled(case: str, import_config: ImportConfig) -> None:
     assert_md_test_case_transformed(test_case=case, transformed_result=result, import_config=import_config)
 
 
-@pytest.mark.parametrize("case", parse_md_test_cases("global_vars_enabled.md"))
+@pytest.mark.parametrize("case", parse_md_test_cases("global_vars_with_ignore_flag.md"))
 def test_global_vars_with_ignored_flag(case: str, import_config: ImportConfig) -> None:
     result: Final = transform_file_content(
-        f"{import_config.import_text}\n{case}", import_config=import_config, ignore_global_vars=False
+        f"{import_config.import_text}\n{case}", import_config=import_config, ignore_global_vars=True
     )
     assert_md_test_case_transformed(test_case=case, transformed_result=result, import_config=import_config)
